4a78ebf9906597e37f2c10356dc60d70ecf9ab0e
Lionel Desorme <lionel.desorme@free.fr>
2023-12-07 12:54:01 +0100

    Update

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,30 @@
+import decimal
+import matplotlib
+import pylab
+import numpy as np
+
+from TestMandel import *
+
+
 print ("Hello world")
+print("plotlib infos: ", matplotlib.__version__, matplotlib.__file__)
 
-a = [ 7, 17, 23]
+def check_plot_lib():
+    #from pylab import *;
+    #pylab.set_loglevel('debug')
+    x = np.arange(0,4*np.pi,0.1)   # start,stop,step
+    y = np.sin(x)
 
-print (a[0])
+
+    pylab.plot(x, y)
+    pylab.show()
+    return
+
+def main():
+    x = 17
+    print (add_one(x))
+    print (Myiter(complex(1,1)))
+    check_plot_lib()
+    
+if __name__ == "__main__":
+    main()
